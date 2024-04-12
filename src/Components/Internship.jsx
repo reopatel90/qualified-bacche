26b90852ff4b33ddc428524f9801dc0fbc7b0f9a
amitkumarit <amiitkumar.it@gmail.com>
2024-04-13 04:28:40 +0530

    course update

--- src/Components/Internship.jsx
+++ src/Components/Internship.jsx
@@ -3,7 +3,7 @@ const Internship = () => {
         <div class="container-xxl py-5">
             <div class="container">
                 <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-                    <h6 class="section-title bg-white text-center text-primary px-3">Courses</h6>
+                    <h6 class="section-title bg-white text-center text-primary px-3">Internship</h6>
                     <h1 class="mb-5">Internship Opportunities</h1>
                 </div>
                 <div class="row g-4 justify-content-center">
